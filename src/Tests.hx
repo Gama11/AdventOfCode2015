fda6d2cb759f945532ee6ae9e4a7a074adaaeab8
Jens Fischer <jensfischer95@gmail.com>
2019-12-20 22:02:37 +0100

    Day 2 part 2

--- src/Tests.hx
+++ src/Tests.hx
@@ -34,5 +34,9 @@ class Tests implements ITest {
 		58 == Day02.calculateWrappingPaperRequirement("2x3x4");
 		43 == Day02.calculateWrappingPaperRequirement("1x1x10");
 		1586300 == Day02.calculateWrappingPaperRequirement(getData("day02"));
+		
+		34 == Day02.calculateRibbonRequirement("2x3x4");
+		14 == Day02.calculateRibbonRequirement("1x1x10");
+		3737498 == Day02.calculateRibbonRequirement(getData("day02"));
 	}
 }
--- src/days/Day02.hx
+++ src/days/Day02.hx
@@ -3,12 +3,23 @@ package days;
 class Day02 {
 	public static function calculateWrappingPaperRequirement(input:String):Int {
 		return input.split("\n").map(function(line) {
-			var s = line.split("x").map(Std.parseInt);
-			var l = s[0];
-			var w = s[1];
-			var h = s[2];
+			var sides = line.split("x").map(Std.parseInt);
+			var l = sides[0];
+			var w = sides[1];
+			var h = sides[2];
 			var sideAreas = [l * w, w * h, h * l];
 			return sideAreas.sum() * 2 + sideAreas.min(i -> i).value;
+		}).sum();
+	}
+
+	public static function calculateRibbonRequirement(input:String):Int {
+		return input.split("\n").map(function(line) {
+			var sides = line.split("x").map(Std.parseInt);
+			var l = sides[0];
+			var w = sides[1];
+			var h = sides[2];
+			sides.sort(Reflect.compare);
+			return (l * w * h) + (sides[0] + sides[1]) * 2;
 		}).sum();
 	}
 }
